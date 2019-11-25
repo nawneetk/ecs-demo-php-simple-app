3cabd0dc32b3270e52a82c3ad36150aea0ab6ed1
nawneetk <57625864+nawneetk@users.noreply.github.com>
2019-11-25 11:55:29 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Simple Network Function</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -15,10 +15,10 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>Simple Network Function</h1>
                 <h2>Congratulations</h2>
                 <p>Your Network Function is now running on a container in Amazon ECS Version 1.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <p>The container is running Network Function <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
